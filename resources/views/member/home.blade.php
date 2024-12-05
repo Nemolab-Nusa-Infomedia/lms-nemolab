7778385504d237904578aeebc6f92a669d66f788
RizqiA17 <asyriansyah@gmail.com>
2024-12-05 15:19:15 +0700

    Update home.blade.php

--- resources/views/member/home.blade.php
+++ resources/views/member/home.blade.php
@@ -194,7 +194,7 @@
                         <ul class="check-active-group mt-3 list-unstyled"> <!-- Changed to ul and added list-unstyled -->
                             <li class="check-active d-flex align-items-center" data-aos="zoom-out">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                                    fill="currentColor" class="bi bi-check-square-fill" style="color: #a3d798;"
+                                    fill="currentColor" class="bi bi-check-square-fill" style="color: hsl(100, 96%, 42.5%);"
                                     viewBox="0 0 16 16">
                                     <path
                                         d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
@@ -204,7 +204,7 @@
                             <li class="check-active d-flex mt-2 align-items-center" data-aos="zoom-out"
                                 data-aos-delay="100">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                                    fill="currentColor" class="bi bi-check-square-fill" style="color: #a3d798;"
+                                    fill="currentColor" class="bi bi-check-square-fill" style="color: hsl(100, 96%, 42.5%);"
                                     viewBox="0 0 16 16">
                                     <path
                                         d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
@@ -214,7 +214,7 @@
                             <li class="check-active d-flex mt-2 align-items-center" data-aos="zoom-out"
                                 data-aos-delay="200">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                                    fill="currentColor" class="bi bi-check-square-fill" style="color: #a3d798;"
+                                    fill="currentColor" class="bi bi-check-square-fill" style="color: hsl(100, 96%, 42.5%);"
                                     viewBox="0 0 16 16">
                                     <path
                                         d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
@@ -224,7 +224,7 @@
                             <li class="check-active d-flex mt-2 align-items-center" data-aos="zoom-out"
                                 data-aos-delay="300">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                                    fill="currentColor" class="bi bi-check-square-fill" style="color: #a3d798;"
+                                    fill="currentColor" class="bi bi-check-square-fill" style="color: hsl(100, 96%, 42.5%);"
                                     viewBox="0 0 16 16">
                                     <path
                                         d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
@@ -232,7 +232,7 @@
                                 <p class="m-0 p-0 ms-2">Konsultasi dengan mentor secara langsung</p>
                             </li>
                         </ul>
-                        <a href="{{ route('member.course') }}" class="btn btn-primary px-4 py-2 mt-4">Gabung Kelas</a>
+                        <a href="{{ route('member.course') }}" class="btn btn-warning px-4 py-2 mt-4">Gabung Kelas</a>
                     </div>
                 </div>
 
